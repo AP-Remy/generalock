b9d23fb58757fe7982a8f06caf584eda1d780e17
AP-Remy <291372869+AP-Remy@users.noreply.github.com>
2026-06-08 16:30:49 +0200

    format et readme

--- script.js
+++ script.js
@@ -5,26 +5,25 @@ const longueur = document.getElementById("longueur");
 const copieur = document.getElementById("copier");
 const affichage = document.getElementById("affichage");
 
-bouton.addEventListener("click", function() {
+bouton.addEventListener("click", function () {
 
-const caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
-const taille = Number(longueur.value);
-const valeurs = new Uint32Array(taille);
-crypto.getRandomValues(valeurs);
-let resultat = "";
-
-for (let i = 0; i < taille; i++){
-    const index = valeurs[i] % caracteres.length;
-    resultat += caracteres[index]
-}
-champ.value = resultat;
+    const caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+    const taille = Number(longueur.value);
+    const valeurs = new Uint32Array(taille);
+    crypto.getRandomValues(valeurs);
+    let resultat = "";
+    for (let i = 0; i < taille; i++) {
+        const index = valeurs[i] % caracteres.length;
+        resultat += caracteres[index];
+    }
+    champ.value = resultat;
 });
 
 
-copieur.addEventListener("click", function() {
-navigator.clipboard.writeText(champ.value);
+copieur.addEventListener("click", function () {
+    navigator.clipboard.writeText(champ.value);
 });
 
-longueur.addEventListener("input", function() {
+longueur.addEventListener("input", function () {
     affichage.textContent = longueur.value + " caractères";
 });
